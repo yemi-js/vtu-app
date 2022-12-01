535e7e386b8d9e4305d80dea109c8b68f76712b3
yemi-js <yemi.js247@gmail.com>
2022-12-01 09:31:23 +0100

    new changes

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>AB GoldSub Dashboard </title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/litepicker/dist/css/litepicker.css" rel="stylesheet" />
+        <link href="{{ asset('dist/css/styles.css')}}" rel="stylesheet" />
+
+        <link rel="icon" type="image/x-icon" href="{{ asset('dist/assets/img/favicon.png')}}" />
+        <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.28.0/feather.min.js" crossorigin="anonymous"></script>
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    </head>
+
+     <body class="nav-fixed">
+
+        @include('layouts.topnavbar')
+
+        @include('layouts.sidenavbar')
+
+        {{-- Scripts --}}
+
+        @yield('content')
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('dist/js/scripts.js')}}"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" crossorigin="anonymous"></script>
+        <script src="{{ asset("dist/assets/demo/chart-area-demo.js")}}"></script>
+        <script src="{{ asset('dist/assets/demo/chart-bar-demo.js')}}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+        <script src="{{asset ('dist/js/datatables/datatables-simple-demo.js')}}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/litepicker/dist/bundle.js" crossorigin="anonymous"></script>
+        <script src="{{ asset('dist/js/litepicker.js')}}"></script>
+     </body>
+
+</html>
---
+++ resources/views/layouts/sidenavbar.blade.php
@@ -0,0 +1,86 @@
+ <div id="layoutSidenav">
+     <div id="layoutSidenav_nav">
+         <nav class="sidenav shadow-right sidenav-dark bg-dark">
+             <div class="sidenav-menu">
+                 <div class="nav accordion" id="accordionSidenav">
+                     <!-- Sidenav Menu Heading (Account)-->
+                     <!-- * * Note: * * Visible only on and above the sm breakpoint-->
+                     <!-- Sidenav Menu Heading (Core)-->
+                     <div class="sidenav-menu-heading">Main</div>
+                     <!-- Sidenav Accordion (Dashboard)-->
+                     <a class="nav-link collapsed" href="/" data-bs-toggle="collapse"
+                         data-bs-target="#collapseDashboards" aria-expanded="false" aria-controls="collapseDashboards">
+                         <div class="nav-link-icon"><i data-feather="activity"  class="text-warning"></i></div>
+                         Dashboard
+
+                     </a>
+
+                     <!-- Sidenav Heading (Custom)-->
+                     <div class="sidenav-menu-heading">Users Management</div>
+                     <!-- Sidenav Accordion (Pages)-->
+                     <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse"
+                         data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                         <div class="nav-link-icon"><i data-feather="users"   class="text-warning"></i></div>
+                         Users List
+
+                     </a>
+                     <!-- Sidenav Accordion (Applications)-->
+                     <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse"
+                         data-bs-target="#collapseApps" aria-expanded="false" aria-controls="collapseApps">
+                         <div class="nav-link-icon"><i data-feather="dollar-sign"  class="text-warning"></i></div>
+                         Fund User Wallet
+                     </a>
+
+                     <!-- Sidenav Accordion (Flows)-->
+                     <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse"
+                         data-bs-target="#collapseFlows" aria-expanded="false" aria-controls="collapseFlows">
+                         <div class="nav-link-icon"><i data-feather="repeat"  class="text-warning"></i></div>
+                         Referral Payout
+                     </a>
+
+                     <!-- Sidenav Heading (UI Toolkit)-->
+                     <div class="sidenav-menu-heading">TRANSACTIONS</div>
+                     <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse"
+                         data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+                         <div class="nav-link-icon"><i data-feather="layout" class="text-warning"></i></div>
+                         Transaction History
+
+                     </a>
+                     <!-- Sidenav Accordion (Applications)-->
+                     <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse"
+                         data-bs-target="#collapseApps" aria-expanded="false" aria-controls="collapseApps">
+                         <div class="nav-link-icon"><i data-feather="dollar-sign"  class="text-warning"></i></div>
+                         Fund User Wallet
+                     </a>
+
+                     <!-- Sidenav Accordion (Flows)-->
+                     <a class="nav-link collapsed" href="javascript:void(0);" data-bs-toggle="collapse"
+                         data-bs-target="#collapseFlows" aria-expanded="false" aria-controls="collapseFlows">
+                         <div class="nav-link-icon"><i data-feather="repeat"  class="text-warning"></i></div>
+                         Referral Payout
+                     </a>
+
+
+                     <!-- Sidenav Heading (Addons)-->
+                     <div class="sidenav-menu-heading">Settings</div>
+                     <!-- Sidenav Link (Charts)-->
+                     <a class="nav-link" href="charts.html">
+                         <div class="nav-link-icon"><i data-feather="user"  class="text-warning"></i></div>
+                         Profile
+                     </a>
+                     <!-- Sidenav Link (Tables)-->
+                     <a class="nav-link" href="tables.html">
+                         <div class="nav-link-icon"><i data-feather="log-out"  class="text-danger"></i></div>
+                         Logout
+                     </a>
+                 </div>
+             </div>
+             <!-- Sidenav Footer-->
+             <div class="sidenav-footer">
+                 <div class="sidenav-footer-content">
+                     <div class="sidenav-footer-subtitle">Logged in as:</div>
+                     <div class="sidenav-footer-title">Valerie Luna</div>
+                 </div>
+             </div>
+         </nav>
+     </div>
